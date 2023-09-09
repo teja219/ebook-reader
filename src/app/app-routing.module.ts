0798e3a105065b118ce7d7c89932b28d0522be80
teja219 <thejesh219@gmail.com>
2023-09-09 17:18:37 +0000

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// Task 1: Add imports here
+import { BookSearchComponent } from './book-search/book-search.component';
+import { BookComponent } from './book/book.component';
 
 const routes: Routes = [
-
-  // Task 1: Add routes here
-
+  { path: '', component: BookSearchComponent },
+  { path: 'book/:individual-book', component: BookComponent },
 ];
 
 @NgModule({
