33d582c25fc462f8b798b8679905d024e3164221
arumat123 <thejesh219@gmail.com>
2023-09-09 17:06:11 -0400

    Display text

--- src/app/book-search/book-search.component.ts
+++ src/app/book-search/book-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../book.service';
+import { IBook } from "../book.model";
+
 // Task 4: import interface here
 
 @Component({
@@ -10,17 +12,46 @@ import { BookService } from '../book.service';
 export class BookSearchComponent implements OnInit {
 
   // Task 4: Declare variables here
+  completeData: any;
+  nothing: boolean = false;
+  books: Array<IBook> = []
+  bookSearchInput: string = ""
 
   constructor(private bookService:BookService) { }
 
   ngOnInit(): void {
-    // Task 4: Call the searchBA() function here
-    }
+      this.searchBA(this.bookSearchInput);
+  }
 
   // Task 4: Add the searchBA() function here
+  searchBA(input: string){
+    this.completeData = []
+    this.books = []
+    this.nothing = false
+    this.bookService.searchBook(input).subscribe(data => {
+		this.completeData = data
+      	for(let result of this.completeData["results"]){
+            this.books.push({
+              "id": result["id"],
+              "download": result["formats"]["text/html"],
+              "author": result["authors"][0].name,
+              "title": result["title"],
+              "image": result["formats"]["image/jpeg"]
+            })
+        }
 
+        if (this.books.length==0){
+          this.nothing = true
+        }
+
+
+
+    })
+  }
   // Task 5: Add the downloadBook() function here
-
+  downloadBook(id: number){
+      window.open("https://www.gutenberg.org/files/" + id + "/" + id + "-0.zip", '_blank');
+  }
 }
 
 
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { BookService } from "../book.service";
+import { ActivatedRoute } from '@angular/router';
 // Task 7: Add import here
 
 @Component({
@@ -9,9 +10,14 @@ import { BookService } from "../book.service";
 })
 export class BookComponent implements OnInit {
   // Task 7: Declare your variables here
+  bookContinuousText: any;
+  bookData: any;
+  bookText: any;
+  textURL!: string;
+  showPre = false;
 
   // Task 8: Declare your variable here
-
+  box: any;
   // Task 11: Declare your variables here
 
   // Task 14: Declare your variable here
@@ -20,19 +26,57 @@ export class BookComponent implements OnInit {
   constructor(
     private bookService: BookService,
     // Task 7: Add private instance here
-
+    private route: ActivatedRoute,
   )
 
   {}
 
   ngOnInit(): void {
     // Task 7: Call your getBook() function here
-
+    this.getBook();
   }
 
   // Task 7: Add your getBook() and toggleDisplay() functions here
-
+  getBook(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.bookService.getBookById(id)
+        .subscribe((data) => {
+          this.bookData = data;
+          if ("text/plain; charset=us-ascii" in this.bookData["formats"]) {
+            this.textURL = this.bookData["formats"]["text/plain; charset=us-ascii"].split('org')[1]
+          }
+          else if ("text/plain; charset=utf-8" in this.bookData["formats"]) {
+            this.textURL = this.bookData["formats"]["text/plain; charset=utf-8"].split('org')[1]
+          }
+          else if ("text/plain; charset=iso-8859-1" in this.bookData["formats"]) {
+            this.textURL = this.bookData["formats"]["text/plain; charset=iso-8859-1"].split('org')[1]
+          }
+          this.bookService.getBookText(this.textURL)
+              .subscribe((textdata) => {
+                this.bookText = textdata;
+                this.bookContinuousText = this.bookText.replaceAll("\r\n\r\n","<br><br>");
+              })
+        })
+  }
+  toggleDisplay() {
+    this.showPre = !this.showPre
+  }
   // Task 8: Add your changeFontSize() function here
+  changeFontSize(newFontSize: string): void {
+    this.box = this.showPre
+        ? document.getElementById("text-pre")
+        : document.getElementById("text");
+
+    if (this.box != null) {
+      this.box.classList.remove(
+          ...["normal-font-size", "large-font-size", "larger-font-size"].filter(
+              (f) => f != newFontSize
+          )
+      );
+      this.box.classList.add(newFontSize);
+    }
+  }
+
 
   // Task 9: Add your changeLineSpacing() function here
 
